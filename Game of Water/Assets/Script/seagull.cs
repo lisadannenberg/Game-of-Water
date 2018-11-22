76ae9edaafd9468ad2b4382064ae33271edddd9e
li_da <li_da@uni-bremen.de>
2018-11-22 23:46:39 +0100

    Seagull Level Start (Backgroud and first moving seagull)

---
+++ Game of Water/Assets/Script/seagull.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class seagull : MonoBehaviour {
+
+	//private int xcoord = 0;
+	//private int ycoord = 0;
+	private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+		rb2d = GetComponent<Rigidbody2D>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+			transform.Translate(-0.1f, 0, 0);
+
+		
+	}
+}
