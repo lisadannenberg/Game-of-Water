91d921592e79708ad2b29811651b81c9160dcbb3
tamzooo <z.tamuna@yahoo.com>
2018-12-06 14:31:50 +0300

    Added Menu functions, button and script to all scenes

---
+++ Game of Water/Assets/Script/Menu/UI.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UI : MonoBehaviour {
+    public GameObject menu;
+    int currentScene;
+	// Use this for initialization
+	void Start ()
+    {
+       
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (SceneManager.GetActiveScene().buildIndex != 2)
+        {
+            currentScene = SceneManager.GetActiveScene().buildIndex;
+        }
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Settings()
+    {
+        menu.gameObject.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void BackToGame()
+    {
+        menu.gameObject.SetActive(false);
+        Time.timeScale = 1f;
+    }
+}
