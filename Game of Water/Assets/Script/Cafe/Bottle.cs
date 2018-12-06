c65a44488d07567d508befa78106c2fc1b32b1f2
LierLoki <sbg@tlu.ee>
2018-12-06 20:02:14 +0300

    By Saba Gotsiridze *Created Level 4 *did PPLSpawn/ Bottles/ Cup scripts *modified DragScript a little

---
+++ Game of Water/Assets/Script/Cafe/Bottle.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bottle : MonoBehaviour {
+    public GameObject[] people;
+    public GameObject[] pplSpawn;
+    public GameObject[] bottles;
+    public GameObject[] bottleSpawn;
+    public GameObject[] txt;
+    public GameObject[] life;
+    public int lifeCounter;
+
+    // Use this for initialization
+    void Start () {
+        bottleSpawn = GameObject.FindGameObjectsWithTag("Spawn");
+        pplSpawn = GameObject.FindGameObjectsWithTag("pplSpawn");
+        txt = GameObject.FindGameObjectsWithTag("score");
+        life = GameObject.FindGameObjectsWithTag("Life1");        
+        
+    }
+
+
+
+    // Update is called once per frame
+    void Update ()
+    {
+        for (int x = 0; x <= life.Length; x++)
+        {
+            if (lifeCounter < life.Length)
+            {
+                lifeCounter++;
+            }
+            
+        }
+    }
+   
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Water")
+        {
+            Destroy(this.gameObject);
+            Destroy(life[lifeCounter - 1]);
+            int y = Random.Range(0, 5);
+            bottles[y].gameObject.GetComponent<Cup>().enabled = true;
+            bottles[y].gameObject.GetComponent<DragScript>().enabled = true;
+            bottles[y].gameObject.GetComponent<PolygonCollider2D>().enabled = true;
+            Instantiate(bottles[y], bottleSpawn[0].transform.position, bottles[y].transform.rotation);
+        }
+        if (collision.gameObject.tag == "Trash")
+        {
+            Destroy(this.gameObject);            
+            //GameObject bot = bottles[Random.Range(0, 5)];     
+            int y = Random.Range(0, 5);
+            bottles[y].gameObject.GetComponent<Cup>().enabled = true;
+            bottles[y].gameObject.GetComponent<DragScript>().enabled = true;
+            bottles[y].gameObject.GetComponent<PolygonCollider2D>().enabled = true;
+            Instantiate(bottles[y], bottleSpawn[0].transform.position, bottles[y].transform.rotation);
+            txt[0].gameObject.GetComponent<score>().count++;
+        }
+
+    }
+}
---
+++ Game of Water/Assets/Script/Cafe/PplSpawn1.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PplSpawn1 : MonoBehaviour {
+    public GameObject[] people;
+    public GameObject[] pplSpawn;
+    public GameObject[] findPerson;
+    public float timeLeft;
+    public bool timer;
+    public int counter;
+    // Use this for initialization
+    void Start () {
+        pplSpawn = GameObject.FindGameObjectsWithTag("pplSpawn");
+        GameObject peo = people[Random.Range(0, 4)];
+        peo.transform.localScale = new Vector3(.5f, .5f, 1);
+        Instantiate(peo, pplSpawn[0].transform.position, peo.transform.rotation);
+        timer = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        findPerson = GameObject.FindGameObjectsWithTag("ppl");
+        counter = findPerson.Length;
+        if (timer == true)
+        {
+            timeLeft -= Time.deltaTime;
+        }
+        if (timer == false)
+        {
+            timeLeft = .5f;
+        }
+        if (timeLeft <= 0)
+        {
+            timer = false;
+            GameObject peo = people[Random.Range(0, 5)];
+            peo.transform.localScale = new Vector3(.5f, .5f, 1);
+            Instantiate(peo, pplSpawn[0].transform.position, peo.transform.rotation);
+            counter = 1;
+            timeLeft = .5f;
+        }
+        if (counter == 0)
+        {
+            timer = true;
+        }
+        
+    }
+    
+}
